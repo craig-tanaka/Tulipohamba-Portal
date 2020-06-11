4cc02280efb611c1b50bffe5e4706455f1e91029
Tanxy Rogue <tanxyrogue@gmail.com>
2020-06-11 10:00:42 +0200

    more responsiveness fixes

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,7 @@
 const sidebar = document.querySelector('section.sidebar');
 const sidebarOverlay = document.querySelector('section.sidebar-overlay');
 const sidebarToggleBtn = document.querySelector('.sidebar-toggle-btn');
+const sidebarBackBtn = document.querySelector('.sidebar-back-btn');
 
 
 sidebarToggleBtn.addEventListener('click', (event) => {
@@ -8,6 +9,10 @@ sidebarToggleBtn.addEventListener('click', (event) => {
     sidebarOverlay.classList.add('active');
 })
 sidebarOverlay.addEventListener('click', (event) => {
+    sidebar.classList.remove('active');
+    sidebarOverlay.classList.remove('active');
+})
+sidebarBackBtn.addEventListener('click', (event) => {
     sidebar.classList.remove('active');
     sidebarOverlay.classList.remove('active');
 })

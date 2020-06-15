f98444614b6df58235743f1f3c3d7b5f28fd220d
Tanxy Rogue <tanxyrogue@gmail.com>
2020-06-15 16:05:22 +0200

    Added User interaction with server

--- public/js/home.js
+++ public/js/home.js
@@ -1,9 +1,23 @@
+const db = firebase.firestore();
+
 const sidebar = document.querySelector('section.sidebar');
 const sidebarOverlay = document.querySelector('section.sidebar-overlay');
 const sidebarToggleBtn = document.querySelector('.sidebar-toggle-btn');
 const sidebarBackBtn = document.querySelector('.sidebar-back-btn');
 
 
+// Get User Account from firebase Auth
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        getUserDetails(user);
+
+    } else {
+        window.location.replace('../index.html');
+    }
+});
+
+
+// #region _____________________EVENT LISTENERS_______________________
 sidebarToggleBtn.addEventListener('click', (event) => {
     sidebar.classList.add('active');
     sidebarOverlay.classList.add('active');
@@ -16,3 +30,50 @@ sidebarBackBtn.addEventListener('click', (event) => {
     sidebar.classList.remove('active');
     sidebarOverlay.classList.remove('active');
 })
+// #endregion _____________________EVENT LISTENERS_______________________
+
+function getUserDetails(user) {
+    db.collection('users').doc(user.uid).get()
+        .then(doc => {
+            console.log(doc.data());
+            addUserSidebarLinks(doc.data());
+        })
+        .catch(err => {
+            window.location.reload();
+        })
+}
+function addUserSidebarLinks(user) {
+    const sidebarLinksElement = document.querySelector('.sidebar-links')
+    const sidebarLinks = {
+        users: `<div class="sidebar-link"> 
+                    <img src = "./img/users-vector.png" class="sidebar-link-img">
+                    <a href="">Users</a> 
+                </div>`,
+        studentElearning:   `<div class="sidebar-link">
+                                <img src="./img/elearning.png" class="sidebar-link-img">
+                                <a href="">Student E-learning</a>
+                            </div>`,
+        academicRecord: `<div class="sidebar-link">
+                            <img src="./img/academic-record-vector.png" class="sidebar-link-img">
+                            <a href="">Academic Record</a>
+                        </div>`,
+        calendar: `<div class="sidebar-link">
+                    <img src="./img/calendar-vector.png" class="sidebar-link-img">
+                    <a href="">Calendar</a>
+                 </div>`,
+        myModules: `<div class="sidebar-link">
+                        <img src="./img/modules-vector.png" class="sidebar-link-img">
+                        <a href="">My Modules</a>
+                    </div>`,
+        announcements: `<div class="sidebar-link">
+                            <img src="./img/announcements-vector.png" class="sidebar-link-img">
+                            <a href="">Announcements</a>
+                        </div>`
+    }
+
+    if (user.accountType === 'Admin') {
+        sidebarLinksElement.innerHTML += sidebarLinks.announcements;
+        sidebarLinksElement.innerHTML += sidebarLinks.calendar;
+        sidebarLinksElement.innerHTML += sidebarLinks.users;
+    }
+}
--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,65 @@
 const signinBtn = document.querySelector('.signin-btn');
 const emailInput = document.querySelector('#signin-email-input')
 const passwordInput = document.querySelector('#signin-password-input')
+const errorDisplay = document.querySelector('.signin-error-display')
+const loader = document.querySelector('.loader-container')
 
 signinBtn.addEventListener('click', (event) => {
-    console.log(firebase.auth())
-    console.log(emailInput.value)
-    console.log(passwordInput.value)
+    // Getting Form Data
+    let email = emailInput.value;
+    let password = passwordInput.value;
+    
+
+    // TODO: add proper form validation
+
+    showLoader();
+    resetInputBorderColors();
+
+    // Login Attempt To firebase
+    firebase.auth().signInWithEmailAndPassword(email, password).then((userCredential) => {
+        window.location = '../home.html'
+    }).catch((error)=>{
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+         if (errorCode === 'auth/invalid-email') {
+             logError('Email address is not valid.');
+             emailInput.style.borderColor = 'red';
+         } else if (errorCode === 'auth/user-disabled') {
+             logError('The user corresponding to the given email has been disabled.');
+             emailInput.style.borderColor = 'red';
+         } else if (errorCode === 'auth/user-not-found') {
+             logError('There is no user corresponding to the given email.');
+             emailInput.style.borderColor = 'red';
+         } else if (errorCode === 'auth/wrong-password') {
+             logError('The password is invalid for the given email, or the account corresponding to the email does not have a password set.');
+             passwordInput.style.borderColor = 'red';
+        }
+        
+        hideLoader();
+    })
 
 
     // window.location = '../home.html';
 })
+
+function logError(logMessage) {
+    errorDisplay.innerHTML = `${logMessage}`;
+}
+function resetInputBorderColors() {
+    emailInput.style.borderColor = '#08355a';
+    passwordInput.style.borderColor = '#08355a';
+}
+function showLoader() {
+    // Hide Submit Button and show loader
+    document.querySelector('.signin-btn-container').classList.toggle('with-loader');
+    loader.style.display = 'initial';
+    signinBtn.style.display = 'none';
+}
+function hideLoader() {
+    // Hide Submit Button and show loader
+    document.querySelector('.signin-btn-container').classList.toggle('with-loader');
+    loader.style.display = 'none';
+    signinBtn.style.display = 'initial';
+}

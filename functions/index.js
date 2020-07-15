2e527abbb107579c4ea4ee804a8e006529f630fd
Tanxy Rogue <tanxyrogue@gmail.com>
2020-07-15 14:16:27 +0200

    added template firebase function

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,23 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.createUser = functions.firestore.document('users-waitlist/{docId}')
+    .onCreate((snap, context) => {
+        const userData = snap.data();
+
+        console.log(userData);
+
+        // admin.auth().createUser({
+        //         uid: 'some-uid',
+        //         email: 'user@example.com',
+        //         phoneNumber: '+11234567890'
+        //     })
+        //     .then(function (userRecord) {
+        //         // See the UserRecord reference doc for the contents of userRecord.
+        //         console.log('Successfully created new user:', userRecord.uid);
+        //     })
+        //     .catch(function (error) {
+        //         console.log('Error creating new user:', error);
+        //     });
+    });

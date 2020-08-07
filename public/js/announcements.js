e0e20422d69c45dcc877d57b05017627fc6ae7b7
Tanxy Rogue <tanxyrogue@gmail.com>
2020-08-07 10:10:33 +0200

    added ability to save announcements to server

--- public/js/announcements.js
+++ public/js/announcements.js
@@ -8,6 +8,8 @@ const attachmentRealInput = document.querySelector('.add-basic-announcement inpu
 const addAnnouncementsSubmitBtn = document.querySelector('.add-basic-announcement .form-input[type="submit"]')
 const addAnnouncementTitleInput = document.querySelector('#announcement-title-input')
 const addAnnouncementContentInput = document.querySelector('#announcement-content-input')
+const createAnnouncementBtn = document.querySelector('.add-basic-announcement .form-input[type="submit"]');
+const loader = document.querySelector('.loader-container');
 
 addAnnouncementsBtn.addEventListener('click', event => {
     addAnnouncementsBtn.style.display = 'none';
@@ -30,11 +32,16 @@ attachmentRealInput.addEventListener('change', event => {
 addAnnouncementsSubmitBtn.addEventListener('click', event => {
     event.preventDefault();
 
-    // Form Validation
-    validateAddAnnouncementsForm();
-
-    // Sending File Attachments to firebase storage 
-    // Send Document to firestore
+    // Send Announcement to server if it has all required data (i.e it has passed validation)  
+    if (validateAddAnnouncementsForm()) {
+        showLoader();
+        loaderLog('Pushing Announcement to server.')
+        if (attachmentRealInput.files.length !== 0) {
+            uploadAnnouncementWithAttachment();
+        } else {
+            uploadAnnouncement();
+        }
+    }
 })
 
 
@@ -62,3 +69,78 @@ function validateAddAnnouncementsForm() {
 
     return hasNoErrors;
 }
+function uploadAttachment(announcementId) {
+    var file = attachmentRealInput.files[0];
+    var storageRef = firebase.storage().ref('Announcement-Attachments/'+ announcementId + '/' + file.name);
+    var task = storageRef.put(file);
+    loaderLog('Uploading Announcement File Attachment')
+    task.on('state_changed', function progress(snapshot) {
+        var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        loaderLog(`File upload Percentage: ${percentage.toFixed(0)}`);
+    }, function error(err) {
+            alert('Attachement Upload Failed. Please Try Again');
+            hideLoader();
+    }, function complete() {
+            loaderLog('file uploaded.');
+            alert('Announcement Uploaded Successfully');
+            hideLoader();
+            addBasicAnnouncementForm.style.display = 'none';
+            addAnnouncementsBtn.style.display = 'block';
+    });
+}
+function uploadAnnouncement() {
+    const db = firebase.firestore();
+
+    db.collection("announcements").add({
+            title: addAnnouncementTitleInput.value,
+            content: addAnnouncementContentInput.value,
+            created: firebase.firestore.FieldValue.serverTimestamp(),
+            hasAttachment: false
+        })
+        .then(function (docRef) {
+            // console.log("Document written with ID: ", docRef.id);
+            loaderLog('Announcement Successfully Pushed To Server');
+            alert('Announcement Uploaded Successfully');
+            hideLoader();
+            addBasicAnnouncementForm.style.display = 'none';
+            addAnnouncementsBtn.style.display = 'block';
+        })
+        .catch(function (error) {
+            // console.error("Error adding document: ", error);
+            loaderLog('Announcement Push to Server failed. Please Try again later');
+            alert('Attachement Upload Failed. Please Try Again');
+            hideLoader();
+        });
+}
+function uploadAnnouncementWithAttachment() {
+    const db = firebase.firestore();
+
+    db.collection("announcements").add({
+            title: addAnnouncementTitleInput.value,
+            content: addAnnouncementContentInput.value,
+            created: firebase.firestore.FieldValue.serverTimestamp(),
+            hasAttachment: true,
+            attachmentFileName: attachmentRealInput.files[0].name
+        })
+        .then(function (docRef) {
+            loaderLog('Announcement Successfully Pushed To Server');
+            uploadAttachment(docRef.id);
+        })
+        .catch(function (error) {
+            // console.error("Error adding document: ", error);
+            loaderLog('Announcement Push to Server failed. Please Try again later');
+        });
+}
+function showLoader() {
+    // Hide Submit Button and show loader
+    createAnnouncementBtn.style.display = 'none';
+    loader.style.display = 'block';
+}
+function hideLoader() {
+    loader.style.display = 'none';
+    createAnnouncementBtn.style.display = 'block';
+}
+function loaderLog(msg){
+    document.querySelector('.loader-log').innerHTML = msg;
+}
+// TODO: Make add announcement title input text bolder
